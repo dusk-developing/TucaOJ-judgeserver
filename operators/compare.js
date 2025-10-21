0aa31b29b411da236d9a2e68ff678afda9e2a975
dusk-developing <duskdev@163.com>
2025-10-21 21:50:23 +0800

    小改进
    在原有的normalize基础上加入了更加精准的比较，考虑到大数据问题，最后选择了token compare的方式

--- operators/compare.js
+++ operators/compare.js
@@ -1,14 +1,34 @@
 const { normalize } = require("../extra_modules/normalize");
 
+function readToken(str, begin) {
+    let end = begin;
+    while (end < str.length && !/\s/.test(str[end])) end++;
+    const token = str.slice(begin, end);
+    return { token, end };
+}
+
 function compareOutput(target, user) {
     let normTarget = normalize(target);
     let normUser = normalize(user);
-    return normTarget === normUser;
+    let tindex = 0, uindex = 0;
+    const tlen = normTarget.length, ulen = normUser.length;
+    while (tindex < tlen && uindex < ulen) {
+        while (tindex < tlen && /\s/.test(normTarget[tindex])) tindex++;
+        while (uindex < ulen && /\s/.test(normUser[uindex])) uindex++;
+        if (tindex >= tlen && uindex >= ulen) return true;
+        if (tindex >= tlen || uindex >= ulen) return false;
+        let ttoken = readToken(normTarget, tindex);
+        let utoken = readToken(normUser, uindex);
+        if (ttoken.token !== utoken.token) return false;
+        tindex = ttoken.end;
+        uindex = utoken.end;
+    }
+    return true;
 }
 
 function compareStrict(target, user) {
     return target === user;
 }
 
-//console.log(compareOutput("Hello\r\r\r\r\r\r\r\r\nWorld", "Hello\nWorld")); true
+console.log(compareOutput("Hello\r\r\r\r\r\r\r\r\n \n    World", "Hello\n   \rWorld")); // true
 module.exports = { compareOutput, compareStrict };
